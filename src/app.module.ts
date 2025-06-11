e46735b4bcc7ac90790bb7d10bbf9d9c3c935af0
Matias Navio <naviomatias2@gmail.com>
2025-06-11 13:38:43 -0300

    Como usar resources, instalación de prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { ProjectsModule } from './projects/projects.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { HelloController } from './hello/hello.controller';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [TasksModule, ProjectsModule, AuthModule, UsersModule],
+  imports: [TasksModule, ProjectsModule, AuthModule, UsersModule, PaymentModule],
   controllers: [HelloController]
 })
 export class AppModule {}
