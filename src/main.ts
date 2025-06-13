627633ad85f6998de57f79dc571615dbf8fb57da
Matias Navio <naviomatias2@gmail.com>
2025-06-13 09:08:26 -0300

    Integración de swagger y cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,19 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
+
+  // integracion de Swagger para documentar la API
+  const config = new DocumentBuilder()
+    .setTitle('Cats example') 
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  // cors para permitir peticiones desde cualquier origen
+  app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,24 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
 
     constructor(private usersService: UsersService) {}
 
+    @ApiResponse({status: 200, description: 'Lista de usuarios'})
     @Get('/all')
     getAllUsers(){
+
         return this.usersService.getAllUsers();
     }
 
     // endpoint para crear un usuario, recibe un objeto de tipo CreateUserDto
     // tiene validacion de datos con ValidationPipe
+    @ApiResponse({status: 201, description: 'Usuario creado exitosamente'})
+    @ApiResponse({status: 400, description: 'Error al crear usuario'})
     @Post('/create')
     crearUser(@Body() user: CreateUserDto){
 
